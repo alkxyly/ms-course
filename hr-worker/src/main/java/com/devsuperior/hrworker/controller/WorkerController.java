ffef4c5c2e476f64fe486c2a06f0c26d45d50f93
alkxyly <alkxyly@hotmail.com>
2022-02-05 17:36:33 -0300

    28 Configurando o Microservico hr-worker como cliente do servidor de configuracao

--- hr-worker/src/main/java/com/devsuperior/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/controller/WorkerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,9 @@ public class WorkerController {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
 	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	private Environment env;
 
@@ -38,4 +42,12 @@ public class WorkerController {
 		logger.info("PORT = "+env.getProperty("local.server.port"));
 		return ResponseEntity.ok(this.workerRepository.findById(id).get());
 	}
+	
+	@GetMapping("/configs")
+	public ResponseEntity<Void> configs(){
+		logger.info("Config = " + this.testConfig);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
 }
