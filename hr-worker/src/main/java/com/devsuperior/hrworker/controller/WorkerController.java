e522a3ba51f17bcad8e42665f8fa096fe48d7936
alkxyly <alkxyly@hotmail.com>
2022-02-05 17:46:25 -0300

    atualizando as configuracoes em tempo de execucao com actuator

--- hr-worker/src/main/java/com/devsuperior/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/controller/WorkerController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.devsuperior.hrworker.entity.Worker;
 import com.devsuperior.hrworker.repository.WorkerRepository;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers", produces = MediaType.APPLICATION_JSON_VALUE)
 public class WorkerController {
