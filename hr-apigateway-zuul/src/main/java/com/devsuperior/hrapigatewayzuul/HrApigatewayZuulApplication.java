457dcef5d92a8c1d776a34fa757c1178f4b19990
alkxyly <alkxyly@hotmail.com>
2022-02-05 15:18:49 -0300

    23. Configuração da API Gateway usando Zull

--- hr-apigateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApigatewayZuulApplication.java
+++ hr-apigateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApigatewayZuulApplication.java
@@ -2,12 +2,24 @@ package com.devsuperior.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
 
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
+@Configuration
 public class HrApigatewayZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrApigatewayZuulApplication.class, args);
 	}
 
+	@Bean
+	public ServerCodecConfigurer serverCodecConfigurer() {
+	   return ServerCodecConfigurer.create();
+	}
 }
