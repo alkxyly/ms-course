8e4cda36cc7185f912f8a756f8b6883c8c42e83a
alkxyly <alkxyly@hotmail.com>
2022-02-24 21:56:27 -0300

    41 Login Jwt P2 - Implementando o endpoint para solocitar o token e configurando o usuario da aplicacao

--- hroauth/hroauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
+++ hroauth/hroauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.devsuperior.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.hroauth.entities.User;
 import com.devsuperior.hroauth.feingclients.UserFeingClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
@@ -23,6 +26,17 @@ public class UserService {
 			throw new IllegalArgumentException("email não encontrado");
 		}
 		logger.info("email encontrado "+email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeingClient.findByEmail(username).getBody();
+		if(user == null) {
+			logger.error("email não encontrado "+username);
+			throw new UsernameNotFoundException("email não encontrado");
+		}
+		logger.info("email encontrado "+username);
 		return user;
 	}
 }
