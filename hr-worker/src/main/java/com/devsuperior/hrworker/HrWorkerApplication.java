40a2f7d1c7fa9478c66dc72fbe87b04f4b58c21f
alkxyly <alkxyly@hotmail.com>
2022-03-05 14:27:54 -0300

    54 referencias para os containers

--- hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
+//@EnableScheduling
 @EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
--- hr-worker/src/main/java/com/devsuperior/hrworker/schedule/ObterDadosScheduling.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/schedule/ObterDadosScheduling.java
@@ -3,10 +3,10 @@ package com.devsuperior.hrworker.schedule;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class ObterDadosScheduling {
 
-	@Scheduled(cron = "10 * * * * *", zone = "America/Sao_Paulo")
+//	@Scheduled(cron = "10 * * * * *", zone = "America/Sao_Paulo")
 	public void executar() {
 		System.out.println("Entrou foi executado com sucesso o scheduled");
 	}
